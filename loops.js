48e1ff35b2ed0bdecbbd1e96ffb3ba9a00af73da
Mary Kang <mary.kang09@gmail.com>
2020-02-13 01:44:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
 for (let i = 0; i<25; i +=1) {
-console.log(i === 1 ? 'I am 1 strange loop.' : `I am ${i} strange loops.`)
+console.log(i === 1 ? 'I am ONE strange loop.' : `I am ${i} strange loops.`)
 }
 }
