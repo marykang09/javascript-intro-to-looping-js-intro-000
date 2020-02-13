c886507d1184e51ee6a4043323f0b5632a96bad3
Mary Kang <mary.kang09@gmail.com>
2020-02-13 04:27:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,11 +14,13 @@ while (n > 0) {
   }
 
 function doWhileLoop(num){
-function incrementVariable() {
-  num = num + 0;
-  return num;
+  var i = 0
+ function incrementVariable() {
+  i = i + 1;
+  return i;
 }
 do {
   console.log("I run once regardless.");
-} while (incrementVariable() < num);
+} while (incrementVariable() <= num);
+return i++;
 }
