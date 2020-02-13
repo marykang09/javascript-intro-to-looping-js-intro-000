87924ed4f61a26ead087c770c74f6c77e1c81682
Mary Kang <mary.kang09@gmail.com>
2020-02-13 03:26:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,11 +4,3 @@ array.push(i === 1 ? 'I am 1 strange loop.' : `I am ${i} strange loops.`);
 }
 return array;
 }
-
-function whileLoop(n){
-let countdown = n;
-return 'done';
-while (whileLoop(n) > 0) {
-  console.log(--countdown);
-}
-}
